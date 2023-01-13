8a61f14e0096a832d7d08fafeabbb7ac633ce3dd
Dmitry <dziamentsyeu.dzmitry@gmail.com>
2023-01-13 21:04:08 +0300

    Working version

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,20 +1,73 @@
 package jm.task.core.jdbc;
-import jm.task.core.jdbc.service.UserServiceImpl;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserServiceImpl userService = new UserServiceImpl();
-        userService.createUsersTable();
-        userService.saveUser("Vasil", "Vasiliev", (byte) 37);
-        userService.saveUser("Ivan", "Ivanov", (byte) 23);
-        userService.saveUser("Petr", "Petrov", (byte) 18);
-        userService.saveUser("Stepan", "Stepanov", (byte) 29);
 
-        userService.getAllUsers();
-        userService.removeUserById(1);
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
+        SessionFactory sessionFactory = Util.getSessionFactory();
+
+        try {
+            Session session = sessionFactory.getCurrentSession();
+
+//Сессия по созданию таблицы
+            session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS users" +
+                            "(id BIGINT PRIMARY KEY AUTO_INCREMENT," +
+                            "name varchar(255) NOT NULL," +
+                            "lastName varchar(255) NOT NULL," +
+                            " age TINYINT NOT NULL" + ")").executeUpdate();
+
+            session.getTransaction().commit();
+
+// Сессия по добавлению данных в таблицу
+            session = sessionFactory.getCurrentSession();
+            User user1 = new User("Bruce", "Lee", (byte) 33);
+            User user2 = new User("Mike", "Tyson", (byte) 54);
+            User user3 = new User("Chuck", "Norris", (byte) 99);
+            User user4 = new User("Jackie", "Chan", (byte) 34);
+            session.beginTransaction();
+            session.save(user1);
+            System.out.println("User с именем – " + user1.getName() +  " добавлен в базу данных");
+            session.save(user2);
+            System.out.println("User с именем – " + user2.getName() +  " добавлен в базу данных");
+            session.save(user3);
+            System.out.println("User с именем – " + user3.getName() +  " добавлен в базу данных");
+            session.save(user4);
+            System.out.println("User с именем – " + user4.getName() +  " добавлен в базу данных");
+            session.getTransaction().commit();
+
+// Сессия по получению всех работников
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            List<User> user = session.createQuery("FROM User ")
+                            .getResultList();
+            for (User u: user) {
+                System.out.println(u);
+            }
+            session.getTransaction().commit();
+
+// //Сессия по очистке таблицы
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            session.createQuery("DELETE FROM  User").executeUpdate();
+            session.getTransaction().commit();
+
+//Сессия по удалению таблицы
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
+            session.getTransaction().commit();
+
+
+        }
+        finally {
+            sessionFactory.close();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,12 +1,16 @@
 package jm.task.core.jdbc.util;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
 
-import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Properties;
 
 public class Util {
     // реализуйте настройку соеденения с БД
@@ -23,6 +27,41 @@ public class Util {
 
         Class.forName(DRIVER);//получаем конкретный драйвер
         return  DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    //Hibernate configuration without XML
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mydbtest?useSSL=false");
+                settings.put(Environment.USER, "root");
+                settings.put(Environment.PASS, "root");
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+
+                settings.put(Environment.SHOW_SQL, "true");
+
+                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                settings.put(Environment.HBM2DDL_AUTO, "");//автоматическое создание таблицы
+
+                configuration.setProperties(settings);
+
+                configuration.addAnnotatedClass(User.class);
+
+                StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 
 }
